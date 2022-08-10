904f232f7f9dc64db9f005d966d1faec27162e5f
Dmitry <vip.mihaslw@mail>
2022-08-10 15:59:13 +0300

    random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,9 @@
-﻿int numberA = 84;
-int numberB = 28;
+﻿int numberA = new Random().Next(1,10); // 1234... 9
+Console.WriteLine("Первое число"); 
+Console.WriteLine(numberA );
+int numberB = new Random().Next(2,3); // 23 25 27... 28
+Console.WriteLine("Второе число");
+Console.WriteLine(numberB );
+Console.WriteLine("Сумма");
 int sum = numberA + numberB;
 Console.WriteLine(sum);
